bf5c4b8693e72c900f918bdf32abb230ca2577ec
testkkj <kimkj1212@naver.com>
2019-01-16 01:58:35 +0900

    chapter02
    첫 번째 프로그램 작성하기

---
+++ ch0201.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+main()
+{
+    printf("코더에게는 작은 한걸음이지만 프로그래머에게는");
+    printf(" 거대한 도약이다!\n");
+    return 0;
+}
---
+++ ch0202.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+main()
+{
+    printf("나는 현재 %c 프로그래밍 언어를 학습하고 있다.\n",'C');
+    printf("나는 지금 막 %d장을 끝냈다.\n",2);
+    printf("나는 현재 %.1f 퍼센트 준비되어 있다. ", 99.9);
+    printf("다음 챕터 학습을 위하여!\n");
+    return 0;
+}
